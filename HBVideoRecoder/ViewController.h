57a672b73f5f81c046d00177bb62ad27f95f4841
H-World <370176334@qq.com>
2017-08-28 22:03:42 +0800

    初始化提交
    提交初始化代码

---
+++ HBVideoRecoder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HBVideoRecoder
+//
+//  Created by 王寒标 on 2017/8/28.
+//  Copyright © 2017年 王寒标. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HBVideoRecoder/ViewController.m
@@ -0,0 +1,21 @@
+//
+//  ViewController.m
+//  HBVideoRecoder
+//
+//  Created by 王寒标 on 2017/8/28.
+//  Copyright © 2017年 王寒标. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
